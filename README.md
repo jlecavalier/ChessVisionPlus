2cf49d1970d85b747ffcfa1f93baf5cd1b554182
Jay LeCavalier <jay.lecavalier@colorado.edu>
2016-04-02 17:12:24 -0600

    messing around with android dialogs. Cool stuff.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # ChessVisionPlus
 App for Android that improves your ability to notice different moves on the chessboard
+
+# NOTE:
+This app is under heavy construction. This is the first app I have ever made. I will likely be messing around with various aspects of android development before I actually start to add the functionality I wanted to in the first place.
--- src/com/jaylecavalier/chessvisionplus/DefaultActivity.java
+++ src/com/jaylecavalier/chessvisionplus/DefaultActivity.java
@@ -1,11 +1,19 @@
 package com.jaylecavalier.chessvisionplus;
 
+// Android stuff
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.support.v4.app.DialogFragment;
 
+// ActionBarSherlock stuff
 import com.actionbarsherlock.app.ActionBar;
 import com.actionbarsherlock.app.SherlockFragmentActivity;
+
+// My stuff
 import com.jaylecavalier.chessvisionplus.R;
+import com.jaylecavalier.chessvisionplus.PushMeDialogFragment;
 
 public class DefaultActivity extends SherlockFragmentActivity {
 	/**
@@ -15,5 +23,13 @@ public class DefaultActivity extends SherlockFragmentActivity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
+	}
+
+	// Called when the button that says "push me" is clicked
+	public void pushMe(View view) {
+		// Handle for the dialog
+		DialogFragment dialog = new PushMeDialogFragment();
+		// Show the dialog to the user, since the button was pressed.
+		dialog.show(getSupportFragmentManager(), "pushme");
 	}
 }
